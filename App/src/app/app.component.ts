d30772ad2b76fba1de467ac111ca55b0a5e433db
CajetanRodrigues <rigrod1999@gmail.com>
2018-09-01 22:30:59 +0530

    Components-Login ,Signup,Training ,LogOut
    Implemented components as mentioned above and added features in it .
    Routing functionality is added so that the user navigates to a particular component smoothly
    Currently Fake Authentication is used to simulate the app. 
    Later on , my plan is to connect with a realtime NoSQL database 'Firebase'

---
+++ App/src/app/app.component.ts
@@ -0,0 +1,23 @@
+// For integration of angular material , refer angular docs on internet
+// For integration of icons , import MatIconModule in material.module.ts which in
+// turn is imported in app.module.ts
+// For Integration of angular flex box , visit github page , click API documentation , and then click
+// 'Using angular cli' link and follow the documentation
+// To enable Form functionalities , import FormsModule
+//
+//Angular Material Setup Docs: https://material.angular.io/guide/getting-started
+//   Angular Material Component Docs: https://material.angular.io/components/categories
+//   Angular Material Github Repo: https://github.com/angular/material2
+// @angular/flex-layout Docs: https://github.com/angular/flex-layout
+//   Flexbox Guide: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+//   Flexbox Video: https://academind.com/learn/css/understanding-css/flexbox-basics-container
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
+
